18a82cf1abfa4b843ca27636d41586ddfeb76d4f
Bryce King <bryceeking5@gmail.com>
2025-01-01 13:03:28 -0500

    Initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/home.jsx"
+
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+function Header(){
+    return(
+        <>
+        <a href="">About</a>
+        <h1>Kingsware</h1>
+        <h2>Establish Your Online Presence</h2>
+        <a href="">Pricing</a>
+        </>
+    )
+}
+
+export default Header
