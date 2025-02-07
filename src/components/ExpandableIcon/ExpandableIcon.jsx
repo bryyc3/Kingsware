b2f98881860f952775811075b0d83cbe7c1ccaec
Bryce King <150635135+bryyc3@users.noreply.github.com>
2025-02-07 15:10:22 +0000

    animations

--- src/components/ExpandableIcon/ExpandableIcon.jsx
+++ src/components/ExpandableIcon/ExpandableIcon.jsx
@@ -5,12 +5,15 @@ import "./ExpandableIcon.css"
 export default function ExpandableIcon({icon}){
     const[expand, setExpand] = useState(false);
     function expandContent(){
-        document.getElementById("expand-container")
+        document.getElementById(icon.icon).classList.add("shrink")
         setExpand(!expand);
+        if(expand){
+            document.getElementById(icon.icon).classList.remove("shrink")
+        }
     }
     return(
         <>
-            <div className="expandable-icon" id="expand-container">
+            <div className="expandable-icon" id={icon.icon}>
                 <button className="material-symbols-outlined expand" onClick={expandContent}>expand_content</button>
                 <div className="icon-container">
                     <span className="material-symbols-outlined icon ">{icon.icon}</span>
